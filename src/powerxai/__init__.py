8ab7422681939d90af19326c9d1c91869453429c
Filip Naudot <filip.naudot@gmail.com>
2026-06-20 21:16:38 +0100

    Add cardinality-based Banzhaf value

--- src/powerxai/__init__.py
+++ src/powerxai/__init__.py
@@ -1,5 +1,5 @@
 from .shapley_value import shapley_value
-from .banzhaf_value import banzhaf_value
+from .banzhaf_value import banzhaf_value, cardinality_banzhaf_value
 from .upsilon_value import upsilon_value
 from .owen_value import owen_value
 from .counterfactual import counterfactual_value, cardinality_counterfactual_value
--- src/powerxai/banzhaf_value.py
+++ src/powerxai/banzhaf_value.py
@@ -1,3 +1,5 @@
+from functools import cache
+from math import comb
 from powerxai.types import Callable, Any
 from powerxai.coalitions import coalitions
 
@@ -39,3 +41,47 @@ def banzhaf_value(player_index: int,
     
     if probabilistic: return total_marginal / (2**(num_players - 1))
     return total_marginal
+
+
+
+
+
+#################
+# CARDINALITY
+#################
+
+@cache
+def _cardinality_banzhaf_weight(cardinality: int, num_players: int) -> float:
+    return (1 / comb(num_players, cardinality) * num_players * comb(num_players - 1, cardinality) / 2**(num_players - 1))
+
+
+def cardinality_banzhaf_value(cardinality: int,
+                              players: list[Any],
+                              value_function: Callable[[list[Any], set[int]], float]
+                              ) -> float:
+    """
+    Compute the cardinality-based Banzhaf value for a given cardinality.
+
+    The cardinality-based Banzhaf value compares the total value of coalitions
+    with size c against coalitions with size c - 1, using the Banzhaf correction
+    for each cardinality layer.
+
+    Args:
+        cardinality (int): Cardinality indicating the layer difference (1 <= c <= n).
+        players (list[Any]): list of players or elements in the reference set.
+        value_function (Callable[[list[Any], set[int]], float]):
+            A function that returns the value of any coalition (based on the player indices).
+
+    Returns:
+        float: The cardinality-based Banzhaf value for the specified cardinality.
+    """
+    num_players = len(players)
+    assert 1 <= cardinality <= num_players, f"cardinality must be in [1, {num_players}]"
+    all_player_indices = set(range(num_players))
+
+    total_value = 0.0
+    for coalition in coalitions(all_player_indices, cardinality=cardinality):
+        total_value += value_function(players, coalition) * _cardinality_banzhaf_weight(cardinality, num_players)
+    for coalition in coalitions(all_player_indices, cardinality=cardinality - 1):
+        total_value -= value_function(players, coalition) * _cardinality_banzhaf_weight(cardinality - 1, num_players)
+    return total_value
